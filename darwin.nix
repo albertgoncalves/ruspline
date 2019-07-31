4b097c8422c317be5f021b463766bad2ba83bfeb
albertgoncalves <goncalves.albert@gmail.com>
2019-07-31 09:11:11 -0400

    add profiling tooling

--- darwin.nix
+++ darwin.nix
@@ -2,6 +2,7 @@ with import <nixpkgs> {};
 mkShell {
     buildInputs = [
         cairo
+        flamegraph
         gtk2
         rustup
         shellcheck
--- linux.nix
+++ linux.nix
@@ -2,6 +2,7 @@ with import <nixpkgs> {};
 mkShell {
     buildInputs = [
         cairo
+        flamegraph
         pkg-config
         rustup
         shellcheck
